b7883941c82dcf7c34f9fb9a00ae5cdef7fe48c9
fauzanpurwaw <fauzanpurwaw@gmail.com>
2024-08-10 18:49:20 +0700

    fix: build error issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-08-09",
   devtools: { enabled: false },
 
+  head: {
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/core-js/3.21.1/minified.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/regenerator-runtime/0.13.7/runtime.min.js' },
+    ],
+  },
+
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -11,14 +18,37 @@ export default defineNuxtConfig({
     },
   },
   modules: [// pinia plugin
-  [
-    "@pinia/nuxt",
-    {
-      storesDirs: ["./stores/**", "./stores/account/**"],
-      autoImports: ['defineStore'],
-    }
-  ], "@nuxt/icon"],
+    [
+      "@pinia/nuxt",
+      {
+        storesDirs: ["./stores/**", "./stores/account/**"],
+        autoImports: ['defineStore'],
+      }
+    ], "@nuxt/icon"],
   imports: {
     dirs: ['./stores'],
   },
+  buildModules: [
+    '@nuxtjs/babel-preset-app',
+  ],
+  vite: {
+    optimizeDeps: {
+      include: ['core-js', 'regenerator-runtime'],
+    },
+  },
+  build: {
+    babel: {
+      presets(env, [preset, options]) {
+        return [["@babel/preset-env", {}]];
+      },
+      plugins: [
+        [
+          "@babel/plugin-transform-runtime",
+          {
+            regenerator: true
+          }
+        ]
+      ]
+    }
+  }
 })
